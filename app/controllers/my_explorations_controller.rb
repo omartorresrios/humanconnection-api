64b8b8deb795ef4b1af54d966dc7d4b5e95d51a6
Omar Torres <omar.torres@fueled.com>
2024-06-30 18:53:55 -0500

    Create new exploration

--- app/controllers/my_explorations_controller.rb
+++ app/controllers/my_explorations_controller.rb
@@ -1,27 +1,32 @@
 class MyExplorationsController < ApplicationController
 
-    # before_action :authenticate_user!
+  # before_action :authenticate_user!
 
-    def all_explorations
-        explorations = Exploration.all#current_user.explorations
-        render json: explorations, each_serializer: ExplorationSerializer, status: 200
-      end
-    
-    #   def update
-    #     note = current_user.notes.find(params[:id])
-    #     note.update(note_params)
-    #     render nothing: true, status: 204
-    #   end
-    
-    #   def create
-    #     new_note = Note.new(note_params)
-    #     new_note.user_id = current_user.id
-    #     new_note.save
-    #     render json: new_note, status: 200
-    #   end
-    
-    #   def destroy
-    #     note = current_user.notes.find(params[:id])
-    #     note.destroy
-    #   end
+  def all_explorations
+      explorations = Exploration.all#current_user.explorations
+      render json: explorations, each_serializer: ExplorationSerializer, status: 200
+    end
+  
+  #   def update
+  #     note = current_user.notes.find(params[:id])
+  #     note.update(note_params)
+  #     render nothing: true, status: 204
+  #   end
+  
+  def create
+    new_exploration = Exploration.new(exploration_params)
+    new_exploration.user_id = 1#current_user.id
+    new_exploration.save
+    render json: new_exploration, status: 200
+  end
+  
+  #   def destroy
+  #     note = current_user.notes.find(params[:id])
+  #     note.destroy
+  #   end
+  private
+
+    def exploration_params
+      params.require(:exploration).permit(:text, sources: [])
+    end
 end
