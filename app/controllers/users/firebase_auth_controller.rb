d73e1a41120407053c3722097b4f8a8282249ef8
Omar Torres <omar.torres@fueled.com>
2024-09-04 16:03:19 -0500

    Remove json format check.

--- app/controllers/users/firebase_auth_controller.rb
+++ app/controllers/users/firebase_auth_controller.rb
@@ -2,13 +2,8 @@ class Users::FirebaseAuthController < ApplicationController
 
   before_action :authorized
   
-    def save_fcm_token
-      if request.format.json?
-        current_user.update_attribute(:fcm_token, request.headers['HTTP_FCM_TOKEN'])
-        render json: { status: 'FCM token saved' }, status: :ok
-      else
-        render json: { error: 'Unsupported format' }, status: :unsupported_media_type
-      end
-    end
+  def save_fcm_token
+    current_user.update_attribute(:fcm_token, request.headers['HTTP_FCM_TOKEN'])
+    render json: { status: 'FCM token saved' }, status: :ok
   end
-  
+end
